9004c620afcf1d2d96c5489c2ba964d70174b6dc
Stefan Stefanov <stefan.stefanov@tenkod.com>
2015-11-01 19:57:56 +0200

    Rest manager login func

--- iOS/TheARM/TheARM/RestManager.m
+++ iOS/TheARM/TheARM/RestManager.m
@@ -11,18 +11,52 @@
 
 @implementation RestManager
 
-static NSString * const apiURL = @"http://api.service.com";
+static NSString * const apiURL = @"http://vm-hackathon2.westeurope.cloudapp.azure.com:8080";
 
 + (void)doLogin:(NSString *) username password:(NSString *) password andToken:(NSString *) token onSuccess:(ARMResponsBlock)success{
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     NSDictionary *parameters = @{@"username": username, @"password": password, @"token":token};
+    NSString *url = [NSString stringWithFormat:@"%@/api/login", apiURL];
     
-    [manager POST:apiURL parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [RestManager doPostRequest:url parameters:parameters onSuccess:success];
+}
+
++ (void)doGetRequest:(NSString *) url parameters:(NSDictionary *) parameters onSuccess:(ARMResponsBlock)success {
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"application/json"];
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    [manager GET:url parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSLog(@"Success: %@", responseObject);
         success(responseObject);
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
     }];
+}
+
++ (void)doPostRequest:(NSString *) url parameters:(NSDictionary *) parameters onSuccess:(ARMResponsBlock)success {
+    AFHTTPRequestOperationManager *manager = [RestManager createManager];
+    
+    [manager POST:url parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Success: %@", responseObject);
+        success(responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+}
+
++ (AFHTTPRequestOperationManager *)createManager {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"application/json"];
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    return manager;
 }
 
 @end
