39f03eb96facf6ee5347169dc4260d12e42c2b62
Peter Georgiev <pepsiko@mail.bg>
2015-11-01 12:51:34 +0200

    Added a few API hadlers in server.js

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,39 @@
  * Copyright Accedia 2015
  */
 var express = require("express");
+var parser = require("body-parser");
+var userController = require("Controllers/usercontroller.js");
+
 var app = express();
+
+app.use(parser.urlencoded(
+{
+    extended : true
+}));
+app.use(parser.json());
+
+app.post('/api/login', function (request, response)
+{
+    var username = request.body.username;
+    var password = request.body.password;
+    var token = request.body.token;
+
+    var returnData = userController.LoginUser(username, password, token);
+
+    request.end(returnData);
+});
+
+app.post('/api/register', function (request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    var token = request.body.token;
+    var displayName = request.body.displayname;
+
+    var returnData = userController.RegisterUser(username, password, token, displayName);
+
+    request.end(returnData);
+});
+
 app.listen(8080, function () {
-    // code here
+    console.log("Service running...");
 });
