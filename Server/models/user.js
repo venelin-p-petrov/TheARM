90fdc21a3ba4ff90c0248e8a8e9b51bd2855b1e8
BlueForeverI <bluef0rever@abv.bg>
2015-11-01 13:50:44 +0200

    Added sserverContext and user model

---
+++ Server/models/user.js
@@ -0,0 +1,11 @@
+﻿/*
+ * User model
+*/
+module.exports = {
+    userId: { type: "integer", required: "true", key: true },
+    email: { type: "text", required: "true" },
+    password: { type: "text", required: "true" },
+    os: { type: "text", required: "true" },
+    displayName: { type: "text", required: "true" },
+    token: { type: "text", required: "true" },
+};
--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,23 @@
  */
 var express = require("express");
 var app = express();
+var constantsModule = require("./constants");
+var orm = require("orm");
+var userModule = require("./models/user");
+var serverContext = require("./models/serverContext");
+
+serverContext()
+    .then(function (models) {
+
+    }, function (error) {
+        console.log("Error: " + JSON.stringify(error));
+});
+
+app.get("/", function (request, response) {
+    response.write("" + JSON.stringify(constantsModule.mySqlUrl()));
+    response.end();
+});
+
 app.listen(8080, function () {
     // code here
 });
