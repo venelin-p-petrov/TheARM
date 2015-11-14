cd8ad767f14e81480f69a724bbb6f31ff4c7f170
JGeorgiev <jgeorgiev@JGeorgievs-MacBook-Pro.local>
2015-11-14 18:44:54 +0200

    Appending client side validation in the Register form

--- iOS/TheARM/TheARM/Tapku/RegisterViewController.m
+++ iOS/TheARM/TheARM/Tapku/RegisterViewController.m
@@ -24,6 +24,14 @@
 
 @implementation RegisterViewController
 
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self changeTextFieldStyles:self.usernameTextField isValid:NO];
+    [self changeTextFieldStyles:self.passwordTextField isValid:NO];
+    [self changeTextFieldStyles:self.confirmPasswordTextField isValid:NO];
+    [self changeTextFieldStyles:self.emailTextField isValid:NO];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -37,18 +45,46 @@
 - (IBAction)registerButtonClicked:(id)sender {
 }
 
-- (IBAction)emailTextFieldEditingEnded:(id)sender {
-    
+- (IBAction)emailEditingChanged:(id)sender {
+    [self changeTextFieldStyles: (UITextField *)sender isValid:[self validateEmail:self.emailTextField.text]];
+}
+
+- (IBAction)fextFieldsMinLenghtEditingChanged:(id)sender {
+    [self changeTextFieldStyles: (UITextField *)sender isValid:((UITextField *)sender).text.length > 6];
+}
+
+- (IBAction)confirmPasswordNotMatchEditingChanged:(id)sender {
+    [self changeTextFieldStyles: (UITextField *)sender isValid: (self.passwordTextField.text == self.confirmPasswordTextField.text && self.confirmPasswordTextField.text.length > 6)];
+}
+
+
+- (void)changeTextFieldStyles:(UITextField *) textField isValid:
+(BOOL) valid {
+    if(!valid){
+        textField.layer.cornerRadius=8.0f;
+        textField.layer.masksToBounds=YES;
+        textField.layer.borderColor=[[UIColor redColor]CGColor];
+        textField.layer.borderWidth= 1.0f;
+    } else{
+        textField.layer.borderWidth= 0.0f;
+    }
 }
 
 - (BOOL)areTextFieldsValid {
     if(self.usernameTextField.text.length > 6 && self.passwordTextField.text.length > 6 &&
-       self.confirmPasswordTextField.text.length >6 && self.confirmPasswordTextField.text == self.passwordTextField.text
-       ){
+       self.confirmPasswordTextField.text.length >6 && self.confirmPasswordTextField.text == self.passwordTextField.text &&
+       [self validateEmail:self.emailTextField.text]){
         return YES;
     } else {
         return NO;
     }
+}
+
+- (BOOL) validateEmail: (NSString *) candidate {
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    
+    return [emailTest evaluateWithObject:candidate];
 }
 
 /*
