486f84222b8901fcd386b0adc312651fd5778f60
m1sh0 <m.n.karev@gmail.com>
2015-11-15 16:41:36 +0200

    add event view controller

---
+++ iOS/TheARM/TheARM/EventViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventViewController.h
+//  TheARM
+//
+//  Created by Mihail Karev on 11/15/15.
+//  Copyright © 2015 Accedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UIViewController
+
+@end
---
+++ iOS/TheARM/TheARM/EventViewController.m
@@ -0,0 +1,13 @@
+//
+//  EventViewController.m
+//  TheARM
+//
+//  Created by Mihail Karev on 11/15/15.
+//  Copyright © 2015 Accedia. All rights reserved.
+//
+
+#import "EventViewController.h"
+
+@implementation EventViewController
+
+@end
